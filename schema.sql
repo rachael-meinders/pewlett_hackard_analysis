064db5b86ae708372c29e625c5257640659c8d46
Rachael M <rachmein17@gmail.com>
2021-11-07 15:17:52 -0600

    completed data creation and structuring

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeeDB
+-- 7.2.2
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
+-- 7.2.2
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -16,6 +18,7 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
+-- 7.2.2
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
@@ -26,6 +29,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+-- 7.2.2
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -54,6 +58,7 @@ CREATE TABLE dept_emp (
 
 SELECT * FROM departments;
 
+-- 7.3.1
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
@@ -74,6 +79,7 @@ SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
 
+-- 7.3.1
 -- Retirement eligibility
 SELECT first_name, last_name
 INTO retirement_info
@@ -81,6 +87,7 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+-- 7.3.1
 -- Number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
@@ -88,3 +95,120 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- 7.3.2
+DROP TABLE retirement_info;
+
+
+-- 7.3.2
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND  '1955-12-31')
+AND (hire_date BETWEEN  '1985-01-01' AND ' 1988-12-31');
+
+-- check the table
+SELECT * FROM retirement_info;
+
+-- 7.3.3
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+-- 7.3.3
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- 7.3.3
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- 7.3.4
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO eligible_currently_empl
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- 7.3.5
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- 7.3.5
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		on (e.emp_no = de.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+	
+-- 7.3.5
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+-- 7.3.5
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+		
+-- 7.3.6
+SELECT * 
+INTO sales_retirement_elig
+FROM dept_info
+WHERE dept_name = 'Sales';
+
+-- 7.3.6
+SELECT * 
+INTO salesdev_retirement_elig
+FROM dept_iNFO
+WHERE dept_name IN ('Sales', 'Development');
