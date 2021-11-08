8fcdca42ee87fd78db66453ca7fee44933bd62c9
rachael-meinders <90879979+rachael-meinders@users.noreply.github.com>
2021-11-08 13:02:37 -0600

    pull current employed eligible employees only

--- Queries/Employee_database_challenge.sql
+++ Queries/Employee_database_challenge.sql
@@ -65,3 +65,37 @@ GROUP BY title
 ORDER BY count DESC;
 
 
+-- select titles of employees eligible for retirement
+DROP TABLE retirement_titles;
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO current_retirement_titles
+FROM employees as e
+JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND  '1955-12-31')
+AND t.to_date = ('9999-01-01')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO current_unique_titles
+FROM current_retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- get # of unique titles 
+--DROP TABLE retiring_titles;
+SELECT COUNT(ut.title), title
+INTO current_retiring_titles
+FROM current_unique_titles as ut
+GROUP BY title
+ORDER BY count DESC;
+
+
